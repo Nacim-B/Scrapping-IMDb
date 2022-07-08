6e3d06a38ea72d840735c9aee0df9b491a72afe4
Nacim B <pctest@macbook-pro-de-pc.home>
2022-07-08 16:27:42 +0200

    actor / movie / director / country files

--- ScrappingIMDbToCsv.py
+++ ScrappingIMDbToCsv.py
@@ -2,22 +2,27 @@ import requests
 import bs4
 from bs4 import BeautifulSoup
 import csv
+import re
 
 url = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250'
 r = requests.get(url)
 soup = bs4.BeautifulSoup(r.text, 'html.parser')
 
-movies_array = []
+movies = []
 
 nbOfMovies = len(soup.find_all('td', {'class': 'titleColumn'}))
 actors = []
+directors = []
+countries = []
 
 for i in range(nbOfMovies):
 
-    dictMovie = {}
+    dictMovie = {"Title": " ", "Year": 0, "Url": "", "Genre": "",
+                 "Directors": [], "Countries": [], "Main Actors": []}
 
     actors_movie = []
     directors_movie = []
+    countries_movie = []
 
     title_movie = soup.find_all('td', {'class': 'titleColumn'})[
         i].find_all('a')[-1].text
@@ -37,10 +42,69 @@ for i in range(nbOfMovies):
         'li', {'class': 'ipc-inline-list__item ipc-chip__text'})[-1].text
     director_movie = movieSoup.find_all(
         'div', {'class': 'ipc-metadata-list-item__content-container'})[0].text
+    country_movie = movieSoup.select(
+        'li[data-testid="title-details-origin"]')[-1].text
+
+    director_movie = re.sub("[(*.)]", "", director_movie)
+    director_movie = re.sub("uncredited", "", director_movie)
+    director_movie = re.sub("co-directed by", "", director_movie)
+    director_movie = re.split(
+        "(?<=[a-zéè])(?<!Mc)(?=[A-ZÉÈÊ])", director_movie)
+    country_movie = re.sub("Country of origin", "", country_movie)
+    country_movie = re.sub("Countries of origin", "", country_movie)
+    country_movie = re.split("(?<=[a-zéè])(?=[A-ZÉÈÊ])", country_movie)
 
     dictMovie['Genre'] = genre_movie
-    dictMovie['Director'] = director_movie
+    dictMovie['Countries'] = countries_movie
+    dictMovie['Directors'] = directors_movie
     dictMovie['Main Actors'] = actors_movie
+
+    nbOfDirectors = len(director_movie)
+    nbOfCountries = len(country_movie)
+
+    for y in range(nbOfCountries):
+        country = {"Name": " ", "Movies": []}
+
+        country_name = country_movie[y]
+
+        countries_movie.append(country_name)
+        country['Name'] = country_name
+        country['Movies'].append(title_movie)
+        country['Number of Movies'] = len(country['Movies'])
+
+        exists = False
+
+        for dict in countries:
+            if dict['Name'] == country_name:
+                dict['Movies'].append(title_movie)
+                dict['Number of Movies'] = len(dict['Movies'])
+                exists = True
+                break
+
+        if not exists:
+            countries.append(country)
+
+    for x in range(nbOfDirectors):
+        dictDirector = {"Name": " ", "Movies": []}
+
+        director_name = director_movie[x]
+
+        directors_movie.append(director_name)
+        dictDirector['Name'] = director_name
+        dictDirector['Movies'].append(title_movie)
+        dictDirector['Number of Movies'] = len(dictDirector['Movies'])
+
+        exists = False
+
+        for dict in directors:
+            if dict['Name'] == director_name:
+                dict['Movies'].append(title_movie)
+                dict['Number of Movies'] = len(dict['Movies'])
+                exists = True
+                break
+
+        if not exists:
+            directors.append(dictDirector)
 
     nbOfActors = len(movieSoup.find_all('a', {'class': 'sc-36c36dd0-1 QSQgP'}))
 
@@ -71,16 +135,30 @@ for i in range(nbOfMovies):
             if not exists:
                 actors.append(actor)
 
-    movies_array.append(dictMovie)
+    movies.append(dictMovie)
+
 
 with open('Top250Movies.csv', 'w', newline="", encoding='utf-8') as f:
-    cols = ['Title', 'Year', 'Url', 'Genre', 'Director', 'Main Actors']
+    cols = ['Title', 'Year', 'Url', 'Genre',
+            'Countries', 'Directors', 'Main Actors']
     writer = csv.DictWriter(f, fieldnames=cols)
     writer.writeheader()
-    writer.writerows(movies_array)
+    writer.writerows(movies)
 
-with open('Top250Movies-Actors.csv', 'w', newline="", encoding='utf-8') as f:
+with open('Top250Movies-Actors.csv', 'w', newline="", encoding='utf-8') as f2:
     cols = ['Name', 'Movies', 'Number of Movies', 'Url']
-    writer = csv.DictWriter(f, fieldnames=cols)
+    writer = csv.DictWriter(f2, fieldnames=cols)
     writer.writeheader()
     writer.writerows(actors)
+
+with open('Top250Movies-Directors.csv', 'w', newline="", encoding='utf-8') as f3:
+    cols = ['Name', 'Movies', 'Number of Movies']
+    writer = csv.DictWriter(f3, fieldnames=cols)
+    writer.writeheader()
+    writer.writerows(directors)
+
+with open('Top250Movies-Countries.csv', 'w', newline="", encoding='utf-8') as f4:
+    cols = ['Name', 'Movies', 'Number of Movies']
+    writer = csv.DictWriter(f4, fieldnames=cols)
+    writer.writeheader()
+    writer.writerows(countries)
